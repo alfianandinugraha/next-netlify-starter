dc7e7af5daad3034a27693ea6f0f609b3816c502
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2021-01-18 18:22:39 +0700

    feat(index): create simple getServerSideProps

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,14 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useEffect } from 'react'
 
-export default function Home() {
+export default function Home(props) {
+
+  useEffect(() => {
+    console.log(props.posts)
+  }, [props.posts])
+
   return (
     <div className="container">
       <Head>
@@ -20,4 +26,14 @@ export default function Home() {
       <Footer />
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then(res => res.json())
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
